98c428e7305bf9757fc9bd03adc074d335acd558
Sankalp Shrivastava <sankalp.shrivastava@autodesk.com>
2025-12-02 20:34:28 +0530

    Refactoring for new infra

---
+++ tests/api/integration_tests/test_suite_ent_list/constants.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2025 Autodesk, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Constants for entity list tests.
+"""
+
+TEST_ENTITY_LIST_ITEMS = {
+    "dd_model": {"entity_type": "T", "items": [56, 57]},
+    "midplane_model": {"entity_type": "T", "items": [56, 57]},
+    "3d_model": {"entity_type": "TE", "items": [3798, 3799]},
+}
--- tests/api/integration_tests/test_suite_ent_list/test_integration_ent_list.py
+++ tests/api/integration_tests/test_suite_ent_list/test_integration_ent_list.py
@@ -11,19 +11,11 @@ of the EntList class with real Moldflow Synergy COM objects.
 import pytest
 from moldflow import EntList, Synergy
 from tests.api.integration_tests.constants import FileSet
-from tests.api.integration_tests.data.set_fields import TEST_ENTITY_LIST_STRING
 
 
 TEST_ENTITY_LIST_PARAMETERS = [
-    ("select_from_string", "items"),
-    ("select_from_predicate", "predicate"),
-    ("select_from_saved_list", "saved_list"),
-]
-
-TEST_ENTITY_LIST_PARAMETERS_IDS = [
-    "select_from_string",
-    "select_from_predicate",
-    "select_from_saved_list",
+    ("select_from_string", "item_string"),
+    ("select_from_predicate", "item_predicate"),
 ]
 
 
@@ -44,20 +36,23 @@ class TestIntegrationEntList:
         return study_doc.create_entity_list()
 
     def _select_entities(
-        self, synergy: Synergy, ent_list: EntList, study_file, select_function, parameter
+        self,
+        synergy: Synergy,
+        ent_list: EntList,
+        study_file,
+        select_function,
+        parameter,
+        values_to_select,
     ):
         """
         Select entities from the entity list.
         """
-        test_entity = TEST_ENTITY_LIST_STRING[study_file]
         if select_function == "select_from_string":
-            getattr(ent_list, select_function)(test_entity[parameter])
+            getattr(ent_list, select_function)(values_to_select[parameter])
         elif select_function == "select_from_predicate":
             pm = synergy.predicate_manager
-            predicate = pm.create_label_predicate(test_entity[parameter])
+            predicate = pm.create_label_predicate(values_to_select[parameter])
             getattr(ent_list, select_function)(predicate)
-        elif select_function == "select_from_saved_list":
-            getattr(ent_list, select_function)(test_entity[parameter])
         else:
             raise ValueError(f"Invalid select function: {select_function}")
 
@@ -71,53 +66,74 @@ class TestIntegrationEntList:
     @pytest.mark.parametrize(
         "select_function, parameter",
         TEST_ENTITY_LIST_PARAMETERS,
-        ids=TEST_ENTITY_LIST_PARAMETERS_IDS,
+        ids=["select_from_string", "select_from_predicate"],
     )
     def test_entity_list_size(
-        self, synergy: Synergy, ent_list: EntList, study_file, select_function, parameter
+        self,
+        synergy: Synergy,
+        ent_list: EntList,
+        study_file,
+        select_function,
+        parameter,
+        expected_values,
     ):
         """
         Test the size property of EntList.
         """
         assert ent_list.size == 0
-        test_entity = TEST_ENTITY_LIST_STRING[study_file]
-        self._select_entities(synergy, ent_list, study_file, select_function, parameter)
-        assert ent_list.size == test_entity["size"]
+        self._select_entities(
+            synergy, ent_list, study_file, select_function, parameter, expected_values
+        )
+        assert ent_list.size == expected_values["size"]
 
     @pytest.mark.parametrize(
         "select_function, parameter",
         TEST_ENTITY_LIST_PARAMETERS,
-        ids=TEST_ENTITY_LIST_PARAMETERS_IDS,
+        ids=["select_from_string", "select_from_predicate"],
     )
     def test_entity_list_convert_to_string(
-        self, synergy: Synergy, ent_list: EntList, study_file, select_function, parameter
+        self,
+        synergy: Synergy,
+        ent_list: EntList,
+        study_file,
+        select_function,
+        parameter,
+        expected_values,
     ):
         """
         Test the convert_to_string method of EntList.
         """
-        test_entity = TEST_ENTITY_LIST_STRING[study_file]
-        self._select_entities(synergy, ent_list, study_file, select_function, parameter)
-        assert ent_list.convert_to_string() == test_entity["converted_string"]
+        self._select_entities(
+            synergy, ent_list, study_file, select_function, parameter, expected_values
+        )
+        assert ent_list.convert_to_string() == expected_values["converted_string"]
 
     @pytest.mark.parametrize(
         "select_function, parameter",
         TEST_ENTITY_LIST_PARAMETERS,
-        ids=TEST_ENTITY_LIST_PARAMETERS_IDS,
+        ids=["select_from_string", "select_from_predicate"],
     )
     def test_entity_list_entity(
-        self, synergy: Synergy, ent_list: EntList, study_file, select_function, parameter
+        self,
+        synergy: Synergy,
+        ent_list: EntList,
+        study_file,
+        select_function,
+        parameter,
+        expected_values,
     ):
         """
         Test the entity method of EntList.
         """
-        test_entity = TEST_ENTITY_LIST_STRING[study_file]
-        self._select_entities(synergy, ent_list, study_file, select_function, parameter)
+        self._select_entities(
+            synergy, ent_list, study_file, select_function, parameter, expected_values
+        )
         for i in range(ent_list.size):
-            assert ent_list.entity(i).convert_to_string() == test_entity["entity"][i]
+            assert ent_list.entity(i).convert_to_string() == expected_values["entity"][str(i)]
 
     def test_entity_list_select_from_predicate_none(self, ent_list: EntList):
         """
-        Test the select_from_predicate method of EntList with None.
+        Test the select_from_saved_list method of EntList.
         """
         ent_list.select_from_predicate(None)
         assert ent_list.size == 0
